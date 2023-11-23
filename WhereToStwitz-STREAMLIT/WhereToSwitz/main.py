9d29f1239ff152169729c2c8877795add9056910
KosaKing <Filip.Kosowski@interia.pl>
2023-11-23 21:38:10 +0100

    Remove tabs and add select box
    This commit removes tabs from the main site to improve the overall user experience.
    Additionally, it introduces a select box for choosing the day, time, and weather options.
    The time selection is now based on the current time, restricting access to past times when selecting today.

--- WhereToStwitz-STREAMLIT/WhereToSwitz/main.py
+++ WhereToStwitz-STREAMLIT/WhereToSwitz/main.py
@@ -3,45 +3,45 @@ from PIL import Image
 import api
 import pandas as pd
 import numpy as np
+from datetime import datetime
 import random
 
 map = Image.open(r"C:\Users\48694\Desktop\CSstudies\PersonalProjects\WhereToSwitz\SwitzCantons.jpg")
+HOURS = ('6:00', '9:00', '12:00', '15:00', '18:00', '21:00')
 
 st.title('WhereToSwitz - Where is a good weather in Switzerland!')
 st.subheader('Map of Switzerland Cantons')
-
 st.image(map)
 
+day_option = st.selectbox(
+   'Choose a day:',
+   ('Today', 'Tomorrow', 'The day after tomorrow'))
 
+if day_option in ('Tomorrow', 'The day after tomorrow'):
+   time_option = st.selectbox('Choose a time:', HOURS)
+else:
+   now = datetime.now()
+   selected_hours = tuple([h for h in HOURS if now.hour < int(h.split(":")[0])])
+   if selected_hours:
+      time_option = st.selectbox('Choose a time:', selected_hours)
+   else:
+      st.text('Check tomorrow')
 
-tab1, tab2, tab3, tab4 = st.tabs(["Overview", "Sun", "Clouds", "Rain"])
+weather_option = st.selectbox(
+   'Choose a weather you are interested in:',
+   ('-', 'Sun', 'Clouds', 'Rain/Snow'))
+# https://openweathermap.org/weather-conditions - this data are going to be helpful to map it all 
 
-with tab1:
-   st.header("Overview")
-   st.dataframe(
-      api.cantonal_data_df,
-      column_config={
-      "Icon": st.column_config.ImageColumn(),
-      },
-      hide_index=True,
-      use_container_width=True,
-      height=945,
-   )
-
-
-with tab2:
-   st.header("Sun")
-   # 20/
-   
-
-with tab3:
-   st.header("Clouds")
-
-with tab4:
-   st.header("Rain/Snow")
-
-
-
+st.header("Overview")
+st.dataframe(
+   api.cantonal_data_df,
+   column_config={
+   "Icon": st.column_config.ImageColumn(),
+   },
+   hide_index=True,
+   use_container_width=True,
+   height=945,
+)
 
 
 # for key, val in api.canton_capital_dict.items():
